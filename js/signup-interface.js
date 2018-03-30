9586532f61db34ddb1f3f0f6bde4d37a81e4e377
Brandon Kudo <brandonkudo@gmail.com>
2018-03-30 15:40:29 -0700

    add button function

--- js/signup-interface.js
+++ js/signup-interface.js
@@ -1,7 +1,30 @@
-var $ = jQuery;
-
 $(document).ready(function() {
+  try {
+    headerToggle();
+    stjornuSlider();
+  } catch(e){
+    console.log(e);
+  }
+});
+
+
+function headerToggle() {
   $('.burger-container').click(function() {
     $('.hide-dropdown').slideToggle();
   });
-});
+}
+
+function stjornuSlider() {
+  let stCounter = 0;
+  let stImgArray = $('.st-image').toArray();
+  $(stImgArray).hide();
+  $(stImgArray[0]).show();
+
+  for (let i = 0; i < stImgArray.length; i++) {
+    $('.carousel-buttons').append('<div class="carousel-button"></div>');
+  }
+
+  let stButtonArray = $('.carousel-button').toArray();
+  $(stButtonArray[0]).addClass('btn-bg-grey');
+
+}
