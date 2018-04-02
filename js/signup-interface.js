e5f5545e829435560add5527fcdb56e44c3e935d
Brandon Kudo <brandonkudo@gmail.com>
2018-04-02 14:36:43 -0700

    add second project

--- js/signup-interface.js
+++ js/signup-interface.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   try {
     headerToggle();
     stjornuSlider();
+    jeffreysSlider();
   } catch(e){
     console.log(e);
   }
@@ -21,10 +22,43 @@ function stjornuSlider() {
   $(stImgArray[0]).show();
 
   for (let i = 0; i < stImgArray.length; i++) {
-    $('.carousel-buttons').append('<div class="carousel-button"></div>');
+    $('.st-carousel-buttons').append('<div class="st-carousel-button"></div>');
   }
 
-  let stButtonArray = $('.carousel-button').toArray();
+  let stButtonArray = $('.st-carousel-button').toArray();
   $(stButtonArray[0]).addClass('btn-bg-grey');
 
+  for (let i = 0; i < stImgArray.length; i++) {
+    $(stButtonArray[i]).click(function() {
+      $(stButtonArray[i]).siblings().removeClass('btn-bg-grey');
+      $(stImgArray[stCounter]).hide();
+      stCounter = i;
+      $(stButtonArray[stCounter]).addClass('btn-bg-grey');
+      $(stImgArray[stCounter]).show();
+    });
+  }
+}
+
+function jeffreysSlider() {
+  let jfCounter = 0;
+  let jfImgArray = $('.jf-image').toArray();
+  $(jfImgArray).hide();
+  $(jfImgArray[0]).show();
+
+  for (let i = 0; i < jfImgArray.length; i++) {
+    $('.jf-carousel-buttons').append('<div class="jf-carousel-button"></div>');
+  }
+
+  let jfButtonArray = $('.jf-carousel-button').toArray();
+  $(jfButtonArray[0]).addClass('btn-bg-grey');
+
+  for (let i = 0; i < jfImgArray.length; i++) {
+    $(jfButtonArray[i]).click(function() {
+      $(jfButtonArray[i]).siblings().removeClass('btn-bg-grey');
+      $(jfImgArray[jfCounter]).hide();
+      jfCounter = i;
+      $(jfButtonArray[jfCounter]).addClass('btn-bg-grey');
+      $(jfImgArray[jfCounter]).show();
+    });
+  }
 }
